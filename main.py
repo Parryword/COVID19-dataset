d410f405e6f5d87efaa12d33a4542b37eafc0dbb
YigitEfeIren <y.iren@featuremind.com>
2025-06-03 17:17:10 +0300

    revert back to hospitalization

--- main.py
+++ main.py
@@ -1,41 +1,36 @@
-from collections import Counter
-
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import cross_val_score, train_test_split
 
 if __name__ == '__main__':
     df = pd.read_csv("covid_related_disease_data.csv")
+
+    # Check for missing values
+    if df.isnull().values.any():
+        missing_info = df.isnull().sum()
+        print(f"Dataset contains missing values:\n{missing_info}")
 
     for col in df.select_dtypes(include='object'):
         df[col] = pd.factorize(df[col])[0]
 
     print(df)
 
-    features = ["Age", "Symptoms", "Severity", "Smoking_Status", "BMI", "Preexisting_Condition", "Hospitalized", "Vaccination_Status"]
-    target = 'Reinfection'
-    x = df[features]
+    features = ["Age", "Symptoms", "Severity", "Smoking_Status", "BMI"]
+    target = 'Hospitalized'
+    X = df[features]
     y = df[target]
-    x_train = x[:500]
-    y_train = y[:500]
-    x_test = x[500:]
-    y_test = y[500:]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     model = RandomForestClassifier()
-    cv = cross_val_score(model, x, y, cv=10)
+    cv = cross_val_score(model, X, y, cv=10)
     print("CV Scores:", cv)
 
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_test)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
     print("Accuracy:", accuracy_score(y_test, y_pred))
 
-    convert = lambda x: ['no' if val == 0 else 'yes' for val in x]
+    convert = lambda x: ['yes' if val == 0 else 'no' for val in x]
 
     y_test_converted = convert(y_test)
     y_pred_converted = convert(y_pred)
